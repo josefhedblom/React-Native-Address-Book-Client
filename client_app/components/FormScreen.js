091d6c8e7cff3560e77829833f5b8f1e413d6e9c
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-17 16:49:52 +0200

    Add state

--- client_app/components/FormScreen.js
+++ client_app/components/FormScreen.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
 export default class FormScreen extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            first_name: '',
+            last_name: '',
+            email: '',
+            phone: '',
+            address: '',
+            city: '',
+            zipCode: '',
+            country: '',
+        }
+    }
     render() {
         return (
             <View>
