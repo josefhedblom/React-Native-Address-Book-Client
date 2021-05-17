387505a037d1467b4c57f40d189010d8dcec1a92
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-17 17:18:40 +0200

    Add post request to FormScreen

--- client_app/components/FormScreen.js
+++ client_app/components/FormScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 import axios from 'axios';
+import {API_URL} from '@env'
 
 export default class FormScreen extends Component {
     constructor(props){
@@ -15,6 +16,26 @@ export default class FormScreen extends Component {
             zipCode: '',
             country: '',
         }
+    }
+    addContact = () => {
+        const newContact = {
+            first_name: this.state.first_name,
+            last_name:  this.state.last_name,
+            email:      this.state.email,
+            phone:      this.state.phone,
+            city:       this.state.city,
+            address:    this.state.address,
+            country:    this.state.country
+        }
+        axios.post(`${API_URL}/add`,newContact)
+        .then(response => {
+            if(response.status === 200) {
+                console.log('response', response);
+            }
+        })
+        .catch(error => {
+            console.log(error.message);
+        })
     }
     render() {
         return (
