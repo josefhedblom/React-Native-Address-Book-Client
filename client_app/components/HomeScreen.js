7d5d1984880a6c442c2910fc8538e1209b60c1e4
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-17 13:22:36 +0200

    Add axios and componentDidMount

--- client_app/components/HomeScreen.js
+++ client_app/components/HomeScreen.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import axios from 'axios';
 
 export default class HomeScreen extends Component {
     constructor(props){
         super(props);
-        this.state = {}
+        this.state = {
+            contactData: []
+        }
+    }
+
+    componentDidMount(){
+        axios.get("http://000.000.0.00:5000") /* ADD IP-ADDRESS TO ENV FILE */
+        .then(response => {
+            this.setState({contactData: response.data});
+        })
+        .catch(error => {
+            console.log(error.message);
+        })
     }
     render() {
         return (
