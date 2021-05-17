96240b83201a27abd3c8cf6ed610340af3ee479e
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-17 18:35:07 +0200

    Add HomeScreen and FormScree to App.js

--- client_app/App.js
+++ client_app/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './components/HomeScreen';
+import FormScreen from './components/FormScreen';
 
 const Stack = createStackNavigator();
 
