99c5d9e14e235c0792806ac22473a9fa6dcfa36d
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-17 17:31:12 +0200

    Add css to form input

--- client_app/components/FormScreen.js
+++ client_app/components/FormScreen.js
@@ -39,29 +39,29 @@ export default class FormScreen extends Component {
     }
     render() {
         return (
-        <View>
-            <View>
+        <View style={styles.container}>
+            <View style={styles.inputField}>
                 <TextInput placeholder="First Name" onChangeText={value => this.setState({first_name: value})}/>
             </View>
-            <View>
+            <View style={styles.inputField}>
                 <TextInput placeholder="Last Name"  onChangeText={value => this.setState({last_name: value})}/>
             </View>
-            <View>
+            <View style={styles.inputField}>
                 <TextInput placeholder="Email"      onChangeText={value => this.setState({email: value})}/>
             </View>
-            <View>
+            <View style={styles.inputField}>
                 <TextInput placeholder="Phone"      onChangeText={value => this.setState({phone: value})}/>
             </View>
-            <View>
+            <View style={styles.inputField}>
                 <TextInput placeholder="Address"    onChangeText={value => this.setState({address: value})}/>
             </View>
-            <View>
+            <View style={styles.inputField}>
                 <TextInput placeholder="City"       onChangeText={value => this.setState({city: value})}/>
             </View>
-            <View>
+            <View style={styles.inputField}>
                 <TextInput placeholder="Country"    onChangeText={value => this.setState({country: value})}/>
             </View>
-            <View>
+            <View style={styles.inputField}>
                 <Button onPress={this.addContact} title="Add Contact"/>
             </View>
         </View>
@@ -69,4 +69,16 @@ export default class FormScreen extends Component {
     }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+    inputField: {
+      width: 300,
+      padding: 20,
+      margin: 10,
+      fontSize:20
+    }
+});
