5c44a87cc6164337df34d2f1d2e55aaf66923d5f
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-21 15:06:13 +0200

    Add useEffect and fetch for API

--- client_app/components/ProfileScreen.js
+++ client_app/components/ProfileScreen.js
@@ -4,6 +4,14 @@ import { View, Text, StyleSheet } from 'react-native';
 import { API_URL } from '@env';
 
 export default function ProfileScreen() {
+
+    useEffect(() => {
+        fetch(`${API_URL}/${itemId}`)
+        .then(response => response.json())
+        .then(data => data.user)
+        .catch(error => console.log(error.message))
+    }, [])
+    
     return (
         <View>
             <Text></Text>
