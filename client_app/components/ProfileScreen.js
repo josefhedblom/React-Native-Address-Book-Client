278451ea2e45204d84ca814f9f16eb09e91ccdb7
Josef Hedblom <hedblomjosef@gmail.com>
2021-05-21 15:01:47 +0200

    Create ProfileScreen

---
+++ client_app/components/ProfileScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useEffect } from 'recat';
+import { View, Text, StyleSheet } from 'react-native';
+import { API_URL } from '@env';
+
+export default function ProfileScreen() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
